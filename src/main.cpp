bd03e17f057a67e3bc913697659cc308507219bc
Grant Yoder <70408179+gyoder@users.noreply.github.com>
2024-01-14 16:38:11 -0700

    updated for the XIAO ESP32S3 and hardware serial

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,17 @@
 #include "flite_arduino.h" // install https://github.com/pschatzmann/arduino-flite
 #include "AudioTools.h" // install https://github.com/pschatzmann/arduino-audio-tools
+#include "HardwareSerial.h"
 //#include "AudioLibs/AudioKit.h"
 
 I2SStream out; // Replace with desired class e.g. AudioKitStream, AnalogAudioStream etc.
 Flite flite(out);
+HardwareSerial TextSerial(1);
 
 
 void setup(){
   Serial.begin(115200);
-  AudioLogger::instance().begin(Serial, AudioLogger::Info);
+  TextSerial.begin(115200, SERIAL_8N1, 5, 6);
+  //AudioLogger::instance().begin(Serial, AudioLogger::None);
 
   // start data sink
   auto cfg = out.defaultConfig();
@@ -16,17 +19,17 @@ void setup(){
   cfg.channels = 1;
   cfg.bits_per_sample = 16;
   cfg.i2s_format = I2S_LSB_FORMAT;
-  cfg.pin_ws = 25;
-  cfg.pin_bck = 2;
-  cfg.pin_data = 26;
+  cfg.pin_ws = 9;
+  cfg.pin_data = 8;
+  cfg.pin_bck = 7;
   out.begin(cfg);
 }
 
 // Arduino loop  
 void loop() {  
-    String sin = Serial.readStringUntil('\n');
-    //Serial.print("Received: ");
-    //Serial.println(sin);
+    String sin = TextSerial.readStringUntil('\n');
+    Serial.print("Received: ");
+    Serial.println(sin);
     if (!sin.isEmpty()) {
         flite.say(sin.c_str());
     }
